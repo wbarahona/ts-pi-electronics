bd0af77ad7b281e47342cc141a1244fd967d490e
Willmer Barahona <wbarahona@live.com>
2020-04-17 12:20:38 -0600

    fix for raspi-io types not defined

--- src/led/simpleled.ts
+++ src/led/simpleled.ts
@@ -1,21 +1,19 @@
 import five, { Board, Led } from 'johnny-five';
-import RaspiLib from 'raspi-io';
-
-const { RaspiIO } = RaspiLib;
+import {RaspiIO as Raspi} from 'raspi-io';
 
 const simpleled = {
     init: () => {
-
-        console.log(1);
         const board: Board = new five.Board({
-            io: new RaspiIO
-        });
-
+            io: new Raspi
+        }); 
 
         board.on('ready', () => {
             const led: Led = new five.Led(13);
 
-            led.blink();
+            console.log(led);
+            
+
+            led.blink(500);
         });
     }
 };
